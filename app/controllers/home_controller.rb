c668f5601915e594d3260c8245feeaf905f6039a
Hiromu Sasaya <h.s.298355@gmail.com>
2019-03-21 21:19:49 +0900

    4

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,23 @@ class HomeController < ShopifyApp::AuthenticatedController
   def index
     @products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
     @webhooks = ShopifyAPI::Webhook.find(:all)
+    # @create_price_rule = ShopifyAPI::PriceRule.new(
+    #     price_rule: {
+    #         title: "FREESHIPPING2",
+    #         target_type: "shipping_line",
+    #         target_selection: "all",
+    #         allocation_method: "each",
+    #         value_type: "percentage",
+    #         value: "-100.0",
+    #         usage_limit: 20,
+    #         customer_selection: "all",
+    #         prerequisite_subtotal_range: {
+    #           greater_than_or_equal_to: "50.0"
+    #         },
+    #         starts_at: "2017-11-19T17:59:10Z"
+    #     }
+    #   )
+    #   @create_price_rule.save
+    @price_rules = ShopifyAPI::PriceRule.find(:all, params:{limit: 10})
   end
 end
--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -3,7 +3,7 @@ ShopifyApp.configure do |config|
   config.api_key = "93423ebd31b84cdd02844403c6d78d55"
   config.secret = "296968c2740ff8345f8293b23e8de904"
   config.old_secret = "<old_secret>"
-  config.scope = "read_products" # Consult this page for more scope options:
+  config.scope = "read_products, write_content, read_themes, write_themes,read_price_rules, write_price_rules" # Consult this page for more scope options:
                                  # https://help.shopify.com/en/api/getting-started/authentication/oauth/scopes
   config.embedded_app = true
   config.after_authenticate_job = false
